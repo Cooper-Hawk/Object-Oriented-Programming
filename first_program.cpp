1c47b42ea3689255df658ffd7bd2766903797028
Cooper-Hawk <chawk7274@floridapoly.edu>
2025-08-20 18:41:58 +0000

    Created first program for class

---
+++ first_program.cpp
@@ -0,0 +1,21 @@
+*
+* 08/20/25
+* The first C++ program
+*
+*
+*/
+
+#include <iostream>
+
+// Create a main function
+int main(void) {
+    std::cout << "Hello COP3337!" << std::endl;
+
+    // std - standard namespace
+    // :: - scope resolution operator
+    // cout - console output (object of an iostream class)
+    // << - stream insertion operator
+    //endl - end of line
+
+    return 0;
+}
